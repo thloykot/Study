d1f5f23fd695f1b2ebc0d9134a04c976703f0e8e
thloykot <61805173+thloykot@users.noreply.github.com>
2021-04-08 19:06:33 +0300

    Abstract class

---
+++ src/main/java/com/company/Nisan.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Nisan extends Car {
+    public void engine(String model_name, String othet_char) {
+        System.out.println(model_name);
+        System.out.println(othet_char);
+    }
+}
