b916fff56556b679b53dc4442f62dfdfdda9ba12
thloykot <61805173+thloykot@users.noreply.github.com>
2021-04-09 13:58:15 +0300

    Delete company directory

--- company/Mercedes.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-import com.entity.Car;
-
-public class Mercedes extends Car {
-    public Mercedes(String engine, String model, int price) {
-        super(engine, model, price);
-    }
-
-}
