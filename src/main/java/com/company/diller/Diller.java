5731ee26c1154963c04863adebd82dbee453d2a3
thloykot <61805173+thloykot@users.noreply.github.com>
2021-07-21 10:18:39 +0300

    Void Test
    DillerTest
    
    88

--- src/main/java/com/company/diller/Diller.java
+++ src/main/java/com/company/diller/Diller.java
@@ -1,10 +1,7 @@
 package com.company.diller;
 
 import com.company.builder.OutputBuilder;
-import com.company.car.Car;
 import com.company.service.CarServise;
-
-import java.util.List;
 
 public class Diller {
     private final CarServise carServise;
